e6069d7697e456b1f3059bb32d2be7554be076ea
Snake Portatil <michael.retamozo@gmail.com>
2021-08-06 15:59:54 +0200

    proyecto finalizado: cambios en el codigo en js. y respuesta final con el resultado de las preguntas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const quizData = [
     {
-        question: '¿Cómo se llama la primera Criptomoneda?',
+        question: '1. ¿Cómo se llama la primera Criptomoneda?',
         a: 'Ethereum',
         b: 'Monero',
         c: 'Bitcoin',
@@ -10,7 +10,7 @@ const quizData = [
         correct: 'c'
     },
     {
-        question: '¿Cómo se llama el metodo de transferencia de datos que usa Bitcoin?',
+        question: '2. ¿Cómo se llama el metodo de transferencia de datos que usa Bitcoin?',
         a: 'SHA1',
         b: 'Blockchain',
         c: 'Tangle',
@@ -18,7 +18,7 @@ const quizData = [
         correct: 'b'
     },
     {
-        question: '¿Cúal es el seudónimo del creador/es de Bitcoin?',
+        question: '3. ¿Cúal es el seudónimo del creador/es de Bitcoin?',
         a: 'Elon Musk',
         b: 'Emurgo\'s',
         c: 'Michael Saylor',
@@ -26,7 +26,7 @@ const quizData = [
         correct: 'd'
     },
     {
-        question: '¿Cúal es la primera cripto en implementar el SmartContract?',
+        question: '4. ¿Cúal es la primera cripto en implementar el SmartContract?',
         a: 'EOS',
         b: 'Ethereum',
         c: 'Binance',
@@ -34,7 +34,7 @@ const quizData = [
         correct: 'b'
     },
     {
-        question: '¿Cúal es el primer país que convierte a Bitcoin en moneda de curso legal?',
+        question: '5. ¿Cúal es el primer país que convierte a Bitcoin en moneda de curso legal?',
         a: 'Suiza',
         b: 'Estados Unidos de America',
         c: 'El Salvador',
@@ -43,6 +43,8 @@ const quizData = [
     }
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -56,6 +58,8 @@ let score = 0;
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+    
     const currentQuizData = quizData[currentQuiz];
 
     questionEl.innerText = currentQuizData.question;
@@ -66,12 +70,11 @@ function loadQuiz() {
 }
 
 function getSelected() {
-    const answersEls = document.querySelectorAll('answer');
-
+    //recoge la respuesta en la input list
     let answer = undefined;
 
-    answersEls.forEach((answerEl) => {
-        if(answer.checked) {
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
             answer = answerEl.id;
         }
     });
@@ -79,26 +82,30 @@ function getSelected() {
     return answer;
 }
 
-function deselectAnswers{
-    
+function deselectAnswers() {
+    //deselecciona la respuesta
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
 }
 
-submitBtn.addEventListener('click', () => {
-    //revisa las respuestas
+submitBtn.addEventListener("click", () => {
+    //valida las respuestas y cuenta el score.
     const answer = getSelected();
 
     if (answer) {
-        if(answer == quizData[currentQuiz].correct){
+        if (answer === quizData[currentQuiz].correct){
             score++;
         }
         currentQuiz++;
-        if(currentQuiz < quizData.length) {
+        if (currentQuiz < quizData.length) {
             loadQuiz();
         } else {
-            // TAREA: Mostrar los resultados.
-            alert("Has acabado!")
+            quiz.innerHTML = `
+            <h2>Has respondido ${score}/${quizData.length} preguntas correctamente.</h2>
+            
+            <button onclick="location.reload()">Recargar</button>
+            `;
         }
     }
-    
-    
 });
